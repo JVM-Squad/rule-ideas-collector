5658608dd2f4b307453a8a8cfe0af5bee39ef598
Angelo Buono <angelo.buono@sonarsource.com>
2023-08-21 16:55:54 +0200

    Initial commit

---
+++ src/main/java/org/sonar/jvm/squad/ruleideascollector/RuleIdeasCollectorApplication.java
@@ -0,0 +1,13 @@
+package org.sonar.jvm.squad.ruleideascollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleIdeasCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleIdeasCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sonar/jvm/squad/ruleideascollector/RuleIdeasCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonar.jvm.squad.ruleideascollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleIdeasCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
