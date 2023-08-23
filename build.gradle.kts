5a3b98a929978f11df95200be46b863cce8604e4
Marco Kaufmann <marco.kaufmann@sonarsource.com>
2023-08-23 10:47:50 +0200

    Removed spring-boot-starter-dpa, added jakarta dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 dependencies {
-	compileOnly("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
--- src/main/java/org/sonar/jvm/squad/ruleideascollector/rest/RuleController.java
+++ src/main/java/org/sonar/jvm/squad/ruleideascollector/rest/RuleController.java
@@ -2,10 +2,10 @@ package org.sonar.jvm.squad.ruleideascollector.rest;
 
 import org.sonar.jvm.squad.ruleideascollector.repo.RuleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RuleController {
 
   @Autowired
