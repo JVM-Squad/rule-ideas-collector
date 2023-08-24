bc66c9343deb373be6bca36d20f636c1b7a79451
Johann Beleites <johann.beleites@sonarsource.com>
2023-08-24 16:17:11 +0200

    First basic frontend experiment

---
+++ src/main/java/org/sonar/jvm/squad/ruleideascollector/client/Controller.java
@@ -0,0 +1,23 @@
+package org.sonar.jvm.squad.ruleideascollector.client;
+
+import java.util.Arrays;
+import org.sonar.jvm.squad.ruleideascollector.rest.RuleController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+  private RuleController ruleController;
+
+  public Controller(RuleController ruleController) {
+    this.ruleController = ruleController;
+  }
+
+
+  @GetMapping("/")
+  public String showRuleList(Model model) {
+    model.addAttribute("rules", ruleController.findAllRules(null, null));
+    return "index";
+  }
+}
